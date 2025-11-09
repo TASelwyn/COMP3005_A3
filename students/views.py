f1c08e46e77aa9b3ad897005e68a6d5300836554
Thomas Selwyn <thomas@selwyn.tech>
2025-11-09 16:51:22 -0500

    Implement addStudent()

--- students/views.py
+++ students/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponseBadRequest
 from .models import Student
-import json
+from django.shortcuts import redirect
 
 
 # Show student UI
@@ -15,3 +15,29 @@ def showStudentUI(request):
 def getAllStudents(request):
     students = Student.objects.all().values()
     return JsonResponse(list(students), safe=False, json_dumps_params={'indent': 4})
+
+# ADD Students
+def addStudent(request):
+    # Must be POST
+    if request.method != "POST":
+        return HttpResponseNotAllowed(["POST"])
+
+    try:
+        # Decode body (x-www-form-urlencoded)
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        enrollment_date = request.POST.get("enrollment_date")
+
+        # Validate input
+        if not all([first_name, last_name, email, enrollment_date]):
+            return HttpResponseBadRequest("Missing required fields")
+        if Student.objects.filter(email=email).exists():
+            return HttpResponseBadRequest("Email already exists.")
+
+        # Create
+        student = Student.objects.create(first_name=first_name, last_name=last_name, email=email, enrollment_date=enrollment_date)
+        return JsonResponse({"student_id": student.student_id, "message": "Successfully added student"})
+
+    except Exception as e:
+        return HttpResponseBadRequest(str(e))
